60e314e33a39ccb780c3da33be4f85b42bd6f729
benoit <benoit.bonnet06@gmail.com>
2016-03-11 16:52:12 +0100

    Mdification of the hitboxes for the diamond bloc

--- headers/diamondBloc.h
+++ headers/diamondBloc.h
@@ -10,7 +10,7 @@
 #include <map>
 
 #define NB_LASERS3 40
-#define EPS_SEC 3 // small distance to add so that the laser issued by the bloc are
+#define EPS_SEC 20 // small distance to add so that the laser issued by the bloc are
                   // issued from the exterior of the hitbox of the code
 
 class diamondBloc: public bloc {
